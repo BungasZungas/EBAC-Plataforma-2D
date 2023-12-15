1d984c49ecdd6cd8f940a6b943104139d3f40d1d
BungasZungas <caiquefloureiro@gmail.com>
2023-12-15 01:45:42 -0300

    fix itemmanager

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -1,22 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ZUNGAS.Core.Singleton;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager : Singleton<ItemManager>
 {
-
-    public static ItemManager instance;
-
     public int coins;
-
-
-    private void Awake()
-    {
-        if (instance == null)
-            instance = this;
-        else
-            Destroy(gameObject);
-    }
 
     private void Start()
     {
